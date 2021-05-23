100aba1323107562fc06c0d430a10f660699fb3a
Voicescu Bogdan <bog.voicescu@gmail.com>
2021-05-23 08:49:48 +0300

    added a visual interface and clickable buttons

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 let playerScore = 0;
 let computerScore = 0;
+
+const rock = document.querySelector("#rock").addEventListener("click", () => {play("rock");});
+const paper = document.querySelector("#paper").addEventListener("click", () => {play("paper");});
+const scissors = document.querySelector("#scissors").addEventListener("click", () => {play("scissors");});
+const info = document.querySelector("#info");
+const scoreboard = document.querySelector("#scoreboard");
+
 function computerPlay(){
     let arr = ["rock", "paper", "scissors"]
     let random = Math.floor(Math.random() * arr.length);
     return arr[random];
 }
-function play(playerSelection, computerSelection){
-    var playerSelection = prompt("Choose: Rock, Paper or Scissors!").toLowerCase();
-    var computerSelection = computerPlay();
+function play(playerSelection){
+    let computerSelection = computerPlay();
     
     if(playerSelection === computerSelection){
-        alert(`Computer chose ${computerSelection}, it's a TIE!`);
+        info.textContent = `Computer chose ${computerSelection}, it's a TIE!`;
     }else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ){
-        alert(`Computer chose ${computerSelection}, you WIN!`);
+        info.textContent = `Computer chose ${computerSelection}, you WIN!`;
         playerScore = playerScore + 1;
-        console.log(`Player score ${playerScore}`)
     }else if(
         (playerSelection === "scissors" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors")
     ){
-        alert(`Computer chose ${computerSelection} you LOSE!`);
+        info.textContent = `Computer chose ${computerSelection} you LOSE!`;
         computerScore = computerScore + 1;
-        console.log(`Computer score: ${computerScore}`);
     }
+    scoreboard.textContent = `Score: You ${playerScore} - Computer ${computerScore}`
 }
-function winner(playerScore, computerScore){
-    if (playerScore == computerScore){
-        alert(`It's a TIE! ${playerScore}-${computerScore}`);
-    }else if(playerScore > computerScore){
-        alert(`You WIN! ${playerScore}-${computerScore}`);
-    }else if(playerScore < computerScore){
-        alert(`You LOSE! ${playerScore}-${computerScore}`);
-    }
-}
-function game(){
-    var gameNr = 0;
-    let i = 0;
-    while (i < 5){
-        play();
-        i++;
-        gameNr = gameNr+1;
-        console.log(`Game nr.${gameNr}`);
-    }
-winner(playerScore, computerScore);
-
-playerScore = 0;
-computerScore = 0;
-}
+// function winner(playerScore, computerScore){
+//     if (playerScore == computerScore){
+//         alert(`It's a TIE! ${playerScore}-${computerScore}`);
+//     }else if(playerScore > computerScore){
+//         alert(`You WIN! ${playerScore}-${computerScore}`);
+//     }else if(playerScore < computerScore){
+//         alert(`You LOSE! ${playerScore}-${computerScore}`);
+//     }
+// }
+// function gameIncr(){
+//     var gameNr = 0;
+//     let i = 0;
+//     while (i < 5){
+//         play();
+//         i++;
+//         gameNr = gameNr+1;
+//         console.log(`Game nr.${gameNr}`);
+//     }
+// winner(playerScore, computerScore);
+// playerScore = 0;
+// computerScore = 0;
+// }
